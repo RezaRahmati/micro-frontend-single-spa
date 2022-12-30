cdc9c908bcb1cf1c143c8ab73eb65038a3cfb4ed
Reza Rahmati <reza.rahmati@pcbank.ca>
2022-12-29 21:00:20 -0500

    fix angular issue

--- sales/src/app/app-routing.module.ts
+++ sales/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +6,9 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/sales' }
+  ]
 })
 export class AppRoutingModule { }
